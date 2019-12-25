e7ae9fcf7748f3ec793870403c33bd2144491e9c
nituzhazha <45794933+nituzhazha@users.noreply.github.com>
2019-12-25 21:25:07 +0800

    delete method RBT

--- src/nn/main.php
+++ src/nn/main.php
@@ -1,9 +1,6 @@
 <?php
-
 namespace nn;
-
 use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\entity\Entity;
@@ -11,62 +8,48 @@ use pocketmine\math\Vector3;
 use pocketmine\utils\Config;
 use nn\Utils\Converter;
 use nn\event\events;
+use nn\Task\FloatingTask;
+use nn\particle\Floating;
 use pocketmine\Server;
-
 class main extends PluginBase{
 	
 	public $npc = null;
 	public $level = null;
 	public $configdir = null;
 	public $single = null;
-
+        public $id = 1000000;
 	
 	public function onEnable(){
-
 		@mkdir($this->getDataFolder());
 		
 if(!is_dir($this->getDataFolder()."data")){
 @mkdir($this->getDataFolder()."data");
 }
-
 if(!file_exists($this->getDataFolder()."skin.png")){
 $this->saveResource("skin.png");
 }
-
-
 		$this->single = $this->getDataFolder()."data/";
 		
-
 		 
 		
-
 for($i=2;$i<count(scandir($this->getDataFolder()."data/"));$i++){
-
 if(count(scandir($this->getDataFolder()."data/"))<=2){
 $this->getLogger()->info("无配置文件加载");
 }else{
 $this->spawnNpc((string)scandir($this->getDataFolder()."data/")[$i]);
-$this->RBT((string)scandir($this->getDataFolder()."data/")[$i]);
+$this->Floating((string)scandir($this->getDataFolder()."data/")[$i]);
+
 
 		}
-
 }
 		
-
        Entity::registerEntity(Npc::class, true);
-
        events::initial($this);
-
      
-
-
-
 $this->getLogger()->info("本插件由nitu一人制作");
 $this->getLogger()->info("作者QQ1010340249");
 $this->getLogger()->info("已开源禁止盗卖");
 $this->getLogger()->info("github项目链接:https://github.com/nituzhazha/Npve");
-
-
 	}
 	
 	
@@ -77,11 +60,12 @@ $this->getLogger()->info("github项目链接:https://github.com/nituzhazha/Npve"
 		
 		
 		if($command->getName()=="npve"){
-
 if(!isset($args[0])){
 		$sender->sendMessage("你没有设置方法");
 		return false;
 	}
+
+if($sender->isOp()){
 
 if($args[0] == "set"){
 	
@@ -113,40 +97,30 @@ if($args[0] == "set"){
            
 		   ]
 		   );#initial data
-
 		$pz->save();
-
-		$this->spawnNpc($args[1]);
-                $this->RBT($args[1]);
+		 $this->spawnNpc($args[1]);
+             
+}
+		}else{
+$sender->sendMessage("§4你没有权限使用");
 }
 
-		}
+
+}
 return true;
 	}
-
-
 	
 	
 	public function spawnNpc($to){
-
               $total = $to;
-
 if(!file_exists($this->single."{$total}/"."config.yml") or !is_dir($this->single."{$total}")) return;
-
-
 		$config = new Config($this->single."{$total}/"."config.yml",Config::YAML);
-
 		$skinResource = $this->single."{$total}/"."skin.png";
-
-
 if(!file_exists($this->single."{$total}/"."skin.png")){
 $skinResource = $this->getDataFolder()."skin.png";
 }
-
-
 $skin =
 Converter::getPngSkin($skinResource);
-
 		 $level = $config->get("level");
 		 $name = $config->get("name");
 		 $health = $config->get("health");
@@ -155,45 +129,40 @@ Converter::getPngSkin($skinResource);
 		 $damagedistance = $config->get("damage-distance");
 		 $size = $config->get("size");
 		 $pos = $config->get("position");
+                 
 	
-
 if($pos == null) return;
-
 $poss = new Vector3($pos["x"], $pos["y"], $pos["z"]);
-
 foreach($this->getServer()->getLevels() as $levels){
 if($levels->getName() == $level){
-
-
-		$npc = new Npc($levels,Entity::createBaseNBT($poss),$skin,$poss,$this->getServer(),$name,$damage,$speed,$damagedistance, $this);
-
-
+		$npc = new Npc($levels,Entity::createBaseNBT($poss),$skin,$poss,$this->getServer(),$name,$damage,$speed,$damagedistance,$this);
         $npc->setScale($size);
         $npc->setMaxHealth($health);
         $npc->setHealth($health);
-
 $npc->spawnToAll();
 		$this->npc = $npc;
 }}
-
 	}
 
-public function RBT($total){
+public function Floating($total){
 
 if(!file_exists($this->single."{$total}/"."config.yml") or !is_dir($this->single."{$total}")) return;
 
-		$config = new Config($this->single."{$total}/"."config.yml",Config::YAML);
-              $level = $config->get("level");
-		 $name = $config->get("name");
-              $time = $config->get("ReBirthTime");
-             
+$config = new Config($this->single."{$total}/"."config.yml",Config::YAML);
+ $pos = $config->get("position");
+$poss = new Vector3($pos["x"], $pos["y"]+1.5, $pos["z"]);
+ $name = $config->get("name");
+$time = $config->get("ReBirthTime");
+$level = $config->get("level");
+$floating = new Floating($this,$poss,$name,$this->id);
 
-$this->getScheduler()->scheduleRepeatingTask(new ReBirthTask($this,$this->npc,$level,$name), $time*20);
+$this->id++;
+$floating->setName($time);
+$this->getScheduler()->scheduleRepeatingTask(new FloatingTask($this,$floating,$name,$time,$level), 20);
+
+
 
 }
 
 
 	}
-
-
-
